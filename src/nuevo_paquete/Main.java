d5ed3ff6d675d58e34796bfd51e558e56c021986
nestor45 <nestoralvarez97@gmail.com>
2022-05-16 11:23:37 -0500

    first commit

---
+++ src/nuevo_paquete/Main.java
@@ -0,0 +1,24 @@
+package nuevo_paquete;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        int numero1 = 12;
+        short numero2 = 65;
+        long numero3 = 324;
+        float numero_decimal1 = 1.5f;
+        double numero_decimal2 = 2.5d;
+        char letra = 'a';
+        String letra2 = "hola mundo";
+        boolean verdad_falso = true;
+
+        System.out.println("Entero: "+ numero1 +"\nShort: "+ numero2 + "\nLong: "+ numero3);
+
+        System.out.println("Float: "+ numero_decimal1 +"\nDouble: "+ numero_decimal2);
+
+        System.out.println("Char: "+ letra +"\nString: "+ letra2);
+
+        System.out.println("Boolean: "+ verdad_falso);
+    }
+    
+}
